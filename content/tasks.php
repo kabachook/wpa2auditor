d74ce3e0a38a5dc82a036c1eba586a81544c9f29
0MazaHacka0 <killerinshadow2@gmail.com>
2017-04-13 21:39:11 +0300

    Add tasks.php

---
+++ content/tasks.php
@@ -0,0 +1,33 @@
+﻿<div class="container">
+  	<h2>Tasks</h2>
+    <div style="overflow: auto;">
+		    <form style="float: left; padding-right: 5px;" action="" class="form-inline" method="POST">
+			       <input type="hidden" name="action" value="finishedtasksdelete">
+			          <input type="submit" value="Delete finished" class="btn btn-default">
+		    </form>
+
+	  <div style="overflow: auto;">
+           <form style="float: left; padding-right: 5px;" action="" class="form-inline" method="POST">
+              <input type="hidden" name="toggleautorefresh" value="On">
+              <input type="submit" value="Turn on auto-reload" class="btn btn-success">
+            </form>
+        <br>
+
+      </div>
+	       <br>
+      </div>
+
+
+      	<div class="panel panel-default">
+      	<table class="table table-striped table-bordered table-nonfluid">
+      		<tbody>
+      			<tr>
+      				<th>ID</th>
+      				<th>Name</th>
+      				<th>Progress</th>
+      				<th>Files</th>
+      			</tr>
+      		</tbody>
+      	</table>
+      	</div>
+      </div>
