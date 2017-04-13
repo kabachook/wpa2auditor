1b6b02078a6a42e02f92347b762154b341ddf8ba
0MazaHacka0 <killerinshadow2@gmail.com>
2017-04-13 21:27:38 +0300

    Forget to add content

---
+++ content/home.php
@@ -0,0 +1,21 @@
+<div class="jumbotron mb0 mx">
+		<div class="container">
+			<h1 class="display-1">WPA2 Auditor</h1>
+			<p>Simple distributed wpa2 handshake bruteforce resistance auditor</p>
+		</div>
+
+	</div>
+	<div class="container">
+		<h1>Our features</h1>
+	</div>
+
+
+	<hr>
+	<div class="container">
+		<div class="alert alert-neutral">
+			<h1>Welcome here!</h1>
+		</div>
+		<footer>
+			Copyright Nick Gant and Atomnijchelovek
+		</footer>
+	</div>
