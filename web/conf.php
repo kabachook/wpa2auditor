247dc4e383dcdd9784dddae2641390ec7270fa0c
0MazaHacka0 <killerinshadow2@gmail.com>
2017-05-06 23:53:29 +0300

    fix path

--- web/conf.php
+++ web/conf.php
@@ -7,18 +7,17 @@ $cfg_db_name='wpa';
 
 //Dicts settings
 //Change to folder where we need to upload dicts
-$cfg_dicts_targetFolder = "C:\wamp64\www\wpa2auditor\web\dicts\\";
+$cfg_dicts_targetFolder = "C:/wamp64/www/wpa2auditor/web/dicts/";
 $cfg_dicts_maxFileSize = 1e+8;
 
 //Site
 $cfg_site_url = 'http://localhost/wpa2auditor/web/';
 
 //Tasks
-$cfg_tasks_targetFolder = "C:\wamp64\www\wpa2auditor\web\\tasks\\";
+$cfg_tasks_targetFolder = "C:/wamp64/www/wpa2auditor/web/tasks/";
 $cfg_tasks_maxFileSize = 1e+8;
 
 //Tools
-$cfg_tools_cap2hccap = "C:\wamp64\www\wpa2auditor\web\\cap2hccap\\cap2hccap.bin";
-$cfg_tools_cap2hccap_tempFilename = "test.hccap";
+$cfg_tools_cap2hccap = "C:/wamp64/www/wpa2auditor/web/cap2hccap/cap2hccap.bin";
 
 ?>
