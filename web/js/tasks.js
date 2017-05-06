bd5a9e255e1b3cedcd3a85bad2204d544b02efb0
0MazaHacka0 <killerinshadow2@gmail.com>
2017-05-06 20:17:46 +0300

    Some fix

--- web/js/tasks.js
+++ web/js/tasks.js
@@ -1,5 +1,13 @@
+/*var temp = $("#ajax").html();
+
+function tasks_ajax_loadPage() {
+	$.post("content/tasks_ajax.php", function (data) {
+		$("#ajax").html(data + temp);
+	});
+}*/
+
 //Chnage class for status
-$(".status").each(function(){
+$(".status").each(function () {
 	if ($(this).text() === "SUCCESS") {
 		$(this).addClass("alert");
 		$(this).addClass("alert-success");
@@ -9,12 +17,12 @@ $(".status").each(function(){
 		$(this).addClass("alert");
 		$(this).addClass("alert-info");
 	}
-	
+
 	if ($(this).text() === "FAILED") {
 		$(this).addClass("alert");
 		$(this).addClass("alert-danger");
 	}
-	
+
 	if ($(this).text() === "IN PROGRESS") {
 		$(this).addClass("alert");
 		$(this).addClass("alert-warning");
