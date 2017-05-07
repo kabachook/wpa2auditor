a8206dfe76f0c0fc79e4bd0bbac62e659e4bc4be
0MazaHacka0 <killerinshadow2@gmail.com>
2017-05-08 00:56:31 +0300

    update cap2hccap to cap2hccapx, update function which get information about handshake for new hccapx standart, update handshake converter (now we can upload cap, hccap, hccapx) and multi cap, hccap, hccapx dump and process them separately, add features that check if handshake uniq, some fixes

--- web/conf.php
+++ web/conf.php
@@ -1,9 +1,9 @@
 <?php
 //DB Configuration
-$cfg_db_host='127.0.0.1';
-$cfg_db_user='wpa';
-$cfg_db_pass='wpapass';
-$cfg_db_name='wpa';
+$cfg_db_host = '127.0.0.1';
+$cfg_db_user = 'wpa';
+$cfg_db_pass = 'wpapass';
+$cfg_db_name = 'wpa';
 
 //Dicts settings
 //Change to folder where we need to upload dicts
@@ -18,6 +18,12 @@ $cfg_tasks_targetFolder = "C:/wamp64/www/wpa2auditor/web/tasks/";
 $cfg_tasks_maxFileSize = 1e+8;
 
 //Tools
-$cfg_tools_cap2hccap = "C:/wamp64/www/wpa2auditor/web/cap2hccap/cap2hccap.bin";
+//Check system on whick php is running, if win use win version of cap2hccapx
+if ( strtoupper( substr( PHP_OS, 0, 3 ) ) === 'WIN' ) {
+	$cfg_tools_cap2hccap = "C:/wamp64/www/wpa2auditor/web/cap2hccap/cap2hccapx.exe";
+} else {
+	$cfg_tools_cap2hccap = "C:/wamp64/www/wpa2auditor/web/cap2hccap/cap2hccapx.bin";
+}
+
 
 ?>
--- web/content/tasks.php
+++ web/content/tasks.php
@@ -21,58 +21,104 @@ function cleanDB() {
 }
 
 //Get all info from handshake in bin and convert it to hex if raw=false
-function getHandshakeInfo( $path, $raw ) {
+function getHandshakeInfo( $path, $raw, $ext ) {
 
 	$hccap = file_get_contents( $path );
-
 	$ahccap = array();
 
-	if ( version_compare( PHP_VERSION, '5.5.0' ) >= 0 ) {
-		$ahccap[ 'essid' ] = unpack( 'Z36', substr( $hccap, 0x000, 36 ) );
+	if ( $ext == "hccap" ) {
+		if ( version_compare( PHP_VERSION, '5.5.0' ) >= 0 ) {
+			$ahccap[ 'essid' ] = unpack( 'Z36', substr( $hccap, 0x000, 36 ) );
+		} else {
+			$ahccap[ 'essid' ] = unpack( 'a36', substr( $hccap, 0x000, 36 ) );
+		}
+		$ahccap[ 'mac1' ] = substr( $hccap, 0x024, 6 );
+		$ahccap[ 'mac2' ] = substr( $hccap, 0x02a, 6 );
+		$ahccap[ 'nonce1' ] = substr( $hccap, 0x030, 32 );
+		$ahccap[ 'nonce2' ] = substr( $hccap, 0x050, 32 );
+		$ahccap[ 'eapol' ] = substr( $hccap, 0x070, 256 );
+		$ahccap[ 'eapol_size' ] = unpack( 'i', substr( $hccap, 0x170, 4 ) );
+		$ahccap[ 'keyver' ] = unpack( 'i', substr( $hccap, 0x174, 4 ) );
+		$ahccap[ 'keymic' ] = substr( $hccap, 0x178, 16 );
+		// fixup unpack
+		$ahccap[ 'essid' ] = $ahccap[ 'essid' ][ 1 ];
+		$ahccap[ 'eapol_size' ] = $ahccap[ 'eapol_size' ][ 1 ];
+		$ahccap[ 'keyver' ] = $ahccap[ 'keyver' ][ 1 ];
+		// cut eapol to right size
+		$ahccap[ 'eapol' ] = substr( $ahccap[ 'eapol' ], 0, $ahccap[ 'eapol_size' ] );
+		// fix order
+		if ( strncmp( $ahccap[ 'mac1' ], $ahccap[ 'mac2' ], 6 ) < 0 )
+			$m = $ahccap[ 'mac1' ] . $ahccap[ 'mac2' ];
+		else
+			$m = $ahccap[ 'mac2' ] . $ahccap[ 'mac1' ];
+		if ( strncmp( $ahccap[ 'nonce1' ], $ahccap[ 'nonce2' ], 6 ) < 0 )
+			$n = $ahccap[ 'nonce1' ] . $ahccap[ 'nonce2' ];
+		else
+			$n = $ahccap[ 'nonce2' ] . $ahccap[ 'nonce1' ];
+		$ahccap[ 'm' ] = $m;
+		$ahccap[ 'n' ] = $n;
+		//return result in hex, else return in bin
+		if ( !$raw ) {
+			$ahccap[ 'mac1' ] = bin2hex( $ahccap[ 'mac1' ] );
+			$ahccap[ 'mac2' ] = bin2hex( $ahccap[ 'mac2' ] );
+			$ahccap[ 'nonce1' ] = bin2hex( $ahccap[ 'nonce1' ] );
+			$ahccap[ 'nonce2' ] = bin2hex( $ahccap[ 'nonce2' ] );
+			$ahccap[ 'eapol' ] = bin2hex( $ahccap[ 'eapol' ] );
+			$ahccap[ 'keymic' ] = bin2hex( $ahccap[ 'keymic' ] );
+		}
 	} else {
-		$ahccap[ 'essid' ] = unpack( 'a36', substr( $hccap, 0x000, 36 ) );
+		$ahccap[ 'signature' ] = substr( $hccap, 0x00, 4 );
+		$ahccap[ 'version' ] = substr( $hccap, 0x04, 4 );
+		$ahccap[ 'message_pair' ] = substr( $hccap, 0x08, 1 );
+		$ahccap[ 'essid_len' ] = substr( $hccap, 0x09, 1 );
+
+		if ( version_compare( PHP_VERSION, '5.5.0' ) >= 0 ) {
+			$ahccap[ 'essid' ] = unpack( 'Z32', substr( $hccap, 0x0a, 32 ) );
+		} else {
+			$ahccap[ 'essid' ] = unpack( 'a32', substr( $hccap, 0x0a, 32 ) );
+		}
+
+		$ahccap[ 'keyver' ] = unpack( 'C', substr( $hccap, 0x2a, 1 ) );
+
+		$ahccap[ 'keymic' ] = substr( $hccap, 0x2b, 16 );
+		$ahccap[ 'mac_ap' ] = substr( $hccap, 0x3b, 6 );
+		$ahccap[ 'nonce_ap' ] = substr( $hccap, 0x41, 32 );
+		$ahccap[ 'mac_sta' ] = substr( $hccap, 0x61, 6 );
+		$ahccap[ 'nonce_sta' ] = substr( $hccap, 0x67, 32 );
+
+		$ahccap[ 'eapol_len' ] = unpack( 'v', substr( $hccap, 0x87, 2 ) );
+
+		$ahccap[ 'eapol' ] = substr( $hccap, 0x89, 256 );
+
+		//Fixup unpack
+
+		$ahccap[ 'essid' ] = $ahccap[ 'essid' ][ 1 ];
+		$ahccap[ 'eapol_len' ] = $ahccap[ 'eapol_len' ][ 1 ];
+		$ahccap[ 'keyver' ] = $ahccap[ 'keyver' ][ 1 ];
+
+		//Cut eapol to right size
+		$ahccap[ 'eapol' ] = substr( $ahccap[ 'eapol' ], 0, $ahccap[ 'eapol_len' ] );
+
+		// fix order
+		if ( strncmp( $ahccap[ 'mac_ap' ], $ahccap[ 'mac_sta' ], 6 ) < 0 )
+			$m = $ahccap[ 'mac_ap' ] . $ahccap[ 'mac_sta' ];
+		else
+			$m = $ahccap[ 'mac_sta' ] . $ahccap[ 'mac_ap' ];
+
+		if ( strncmp( $ahccap[ 'nonce_ap' ], $ahccap[ 'nonce_sta' ], 6 ) < 0 )
+			$n = $ahccap[ 'nonce_ap' ] . $ahccap[ 'nonce_sta' ];
+		else
+			$n = $ahccap[ 'nonce_sta' ] . $ahccap[ 'nonce_ap' ];
+
+		$ahccap[ 'm' ] = $m;
+		$ahccap[ 'n' ] = $n;
+
+		//If raw false
+		if ( !$raw ) {
+			$ahccap[ 'mac_ap' ] = bin2hex( $ahccap[ 'mac_ap' ] );
+		}
 	}
-
-	$ahccap[ 'mac1' ] = substr( $hccap, 0x024, 6 );
-	$ahccap[ 'mac2' ] = substr( $hccap, 0x02a, 6 );
-	$ahccap[ 'nonce1' ] = substr( $hccap, 0x030, 32 );
-	$ahccap[ 'nonce2' ] = substr( $hccap, 0x050, 32 );
-	$ahccap[ 'eapol' ] = substr( $hccap, 0x070, 256 );
-	$ahccap[ 'eapol_size' ] = unpack( 'i', substr( $hccap, 0x170, 4 ) );
-	$ahccap[ 'keyver' ] = unpack( 'i', substr( $hccap, 0x174, 4 ) );
-	$ahccap[ 'keymic' ] = substr( $hccap, 0x178, 16 );
-
-	// fixup unpack
-	$ahccap[ 'essid' ] = $ahccap[ 'essid' ][ 1 ];
-	$ahccap[ 'eapol_size' ] = $ahccap[ 'eapol_size' ][ 1 ];
-	$ahccap[ 'keyver' ] = $ahccap[ 'keyver' ][ 1 ];
-
-	// cut eapol to right size
-	$ahccap[ 'eapol' ] = substr( $ahccap[ 'eapol' ], 0, $ahccap[ 'eapol_size' ] );
-
-	// fix order
-	if ( strncmp( $ahccap[ 'mac1' ], $ahccap[ 'mac2' ], 6 ) < 0 )
-		$m = $ahccap[ 'mac1' ] . $ahccap[ 'mac2' ];
-	else
-		$m = $ahccap[ 'mac2' ] . $ahccap[ 'mac1' ];
-
-	if ( strncmp( $ahccap[ 'nonce1' ], $ahccap[ 'nonce2' ], 6 ) < 0 )
-		$n = $ahccap[ 'nonce1' ] . $ahccap[ 'nonce2' ];
-	else
-		$n = $ahccap[ 'nonce2' ] . $ahccap[ 'nonce1' ];
-
-	$ahccap[ 'm' ] = $m;
-	$ahccap[ 'n' ] = $n;
-
-	//return result in hex, else return in bin
-	if ( !$raw ) {
-		$ahccap[ 'mac1' ] = bin2hex( $ahccap[ 'mac1' ] );
-		$ahccap[ 'mac2' ] = bin2hex( $ahccap[ 'mac2' ] );
-		$ahccap[ 'nonce1' ] = bin2hex( $ahccap[ 'nonce1' ] );
-		$ahccap[ 'nonce2' ] = bin2hex( $ahccap[ 'nonce2' ] );
-		$ahccap[ 'eapol' ] = bin2hex( $ahccap[ 'eapol' ] );
-		$ahccap[ 'keymic' ] = bin2hex( $ahccap[ 'keymic' ] );
-	}
+	$ahccap[ 'ext' ] = $ext;
 
 	return $ahccap;
 }
@@ -82,12 +128,16 @@ function check_key( $id, $key ) {
 	global $mysqli;
 
 	//Get filename for task
-	$sql = "SELECT server_path FROM tasks WHERE id='" . $id . "'";
-	$server_path = $mysqli->query( $sql )->fetch_object()->server_path;
+	$sql = "SELECT server_path, ext FROM tasks WHERE id='" . $id . "'";
+	$result = $mysqli->query( $sql )->fetch_object();
+	$server_path = $result->server_path;
+	$ext = $result->ext;
 
-	//Get handshake info in raw
-	$ahccap = getHandshakeInfo( $server_path, true );
-	$block = "Pairwise key expansion\0" . $ahccap[ 'm' ] . $ahccap[ 'n' ] . "\0";
+	//Get handshake info 
+	$ahccap = getHandshakeInfo( $server_path, true, $ext );
+	$m = $ahccap[ 'm' ];
+	$n = $ahccap[ 'n' ];
+	$block = "Pairwise key expansion\0" . $m . $n . "\0";
 
 	$pmk = hash_pbkdf2( 'sha1', $key, $ahccap[ 'essid' ], 4096, 32, True );
 	$ptk = hash_hmac( 'sha1', $block, $pmk, True );
@@ -107,50 +157,109 @@ function handshakeConverter( $file ) {
 	global $cfg_tools_cap2hccap;
 
 	$name = $file[ 'taskName' ];
-	$path = $file[ 'server_path' ];
+	$path = $file[ 'server_path_toFile' ];
 	$extension = $file[ 'ext' ];
-	$output = $path;
+	$output = $file[ 'server_path' ] . $file[ 'fileName' ];
 
-	//Convert cap to hccap
+	//Convert cap to hccapx
 	if ( $extension == "cap" ) {
-
-		//Execute hccap
+		$output = $file[ 'server_path' ] . $file[ 'fileName' ] . ".hccapx";
+		//Execute cap2hccapx
 		exec( $cfg_tools_cap2hccap . " " . $path . " " . $output );
+		$extension = "hccapx";
 	}
 
-	$size = filesize( $path );
-	if ( $size == 392 ) {
-		return array( $path );
-	} elseif ( $size % 392 == 0 ) {
-		//Slice file
+	$size = filesize( $output );
+	$ret = array();
+
+	if ( $extension == "hccap" ) {
+		if ( $size == 392 ) {
+			array_push( $ret, array( "ext" => "hccap", "path" => $output, "name" => $file[ 'fileName' ] . ".hccap" ) );
+		} elseif ( $size % 392 == 0 ) {
+			//Slice file
+			$original = file_get_contents( $output );
+			$offset = 0;
+
+			for ( $i = 0; $i < ( $size / 392 ); $i++ ) {
+				$sliced = substr( $original, $offset, 392 );
+				$offset += 392;
+				$out = $output . "_" . ( $i + 1 ) . ".hccap";
+				fwrite( fopen( $out, "w" ), $sliced );
+				array_push( $ret, array( "ext" => "hccap", "path" => $out, "name" => $file[ 'fileName' ] . ".hccap" . "_" . ( $i + 1 ) . ".hccap" ) );
+			}
+		} else {
+			return NULL;
+		}
 	} else {
-		return NULL;
+		if ( $size == 393 ) {
+			array_push( $ret, array( "ext" => "hccapx", "path" => $output, "name" => $file[ 'fileName' ] . ".hccapx" ) );
+		} elseif ( $size % 393 == 0 ) {
+			//Slice file
+			$original = file_get_contents( $output );
+			$offset = 0;
+
+			for ( $i = 0; $i < ( $size / 393 ); $i++ ) {
+				$sliced = substr( $original, $offset, 393 );
+				$offset += 393;
+				$out = $output . "_" . ( $i + 1 ) . ".hccapx";
+				fwrite( fopen( $out, "w" ), $sliced );
+				array_push( $ret, array( "ext" => "hccapx", "path" => $out, "name" => $file[ 'fileName' ] . ".hccapx" . "_" . ( $i + 1 ) . ".hccapx" ) );
+			}
+		} else {
+			return NULL;
+		}
 	}
 
+	return $ret;
 }
 
 function addTaskToDB( $file, $info ) {
 	global $mysqli;
+	global $status_file_uploading;
 
-	$server_path = $file[ 'server_path' ];
+	$server_path = $file[ 'server_path_toFile' ];
 	$name = $file[ 'taskName' ];
 	$filename = $file[ 'fileName' ];
 	$site_path = $file[ 'site_path' ] . $filename;
 
-	//Add task to db
-	$thash = hash_file( "sha256", $server_path );
-	$sql = "INSERT INTO tasks(name, type, priority, filename, thash, essid, station_mac, server_path, site_path) VALUES('" . $name . "', '0', '0', '" . $filename . "', UNHEX('" . $thash . "'), '" . $info[ 'essid' ] . "', '" . $info[ 'mac1' ] . "', '" . $server_path . "', '" . $site_path . "')";
-	$mysqli->query( $sql );
+	//Check if handshake uniq
 
-	//Get all dicts id
-	$sql = "SELECT id FROM dicts";
-	$result = $mysqli->query( $sql )->fetch_all( MYSQLI_ASSOC );
+	if ( $info[ 'ext' ] == "hccap" ) {
+		$curr_hand_hash = md5( $info[ "keymic" ] . $info[ 'mac1' ] . $info[ 'essid' ] );
+	} else {
+		$curr_hand_hash = md5( $info[ "keymic" ] . $info[ 'mac_ap' ] . $info[ 'essid' ] );
+	}
 
-	//Insert into tasks_dicts for last (current) task all dicts
-	foreach ( $result as $row ) {
-		$dict_curr_id = $row[ 'id' ];
-		$sql = "INSERT INTO tasks_dicts(net_id, dict_id, status) VALUES('" . getLastNetID() . "', '" . $dict_curr_id . "', '0')";
+	$sql = "SELECT * FROM tasks WHERE uniq_hash=UNHEX('" . $curr_hand_hash . "')";
+	$result = $mysqli->query( $sql );
+	if ( $result->num_rows != 0 ) {
+		//Hash is not uniq
+		$result = $result->fetch_object();
+		$status_file_uploading = '<td><div class="alert alert-danger mb0" role="alert">Hash already in DB. Password : ' . $result->net_key . '</div></td>';
+	} else {
+		//So hash is uniq
+
+		if ( $info[ 'ext' ] == "hccap" ) {
+			$mac = $info[ 'mac1' ];
+		} else {
+			$mac = $info[ 'mac_ap' ];
+		}
+
+		//Add task to db
+		$thash = hash_file( "sha256", $server_path );
+		$sql = "INSERT INTO tasks(name, type, priority, filename, thash, essid, station_mac, server_path, site_path, uniq_hash, ext) VALUES('" . $name . "', '0', '0', '" . $filename . "', UNHEX('" . $thash . "'), '" . $info[ 'essid' ] . "', '" . $mac . "', '" . $server_path . "', '" . $site_path . "', UNHEX('" . $curr_hand_hash . "'), '" . $info[ 'ext' ] . "')";
 		$mysqli->query( $sql );
+
+		//Get all dicts id
+		$sql = "SELECT id FROM dicts";
+		$result = $mysqli->query( $sql )->fetch_all( MYSQLI_ASSOC );
+
+		//Insert into tasks_dicts for last (current) task all dicts
+		foreach ( $result as $row ) {
+			$dict_curr_id = $row[ 'id' ];
+			$sql = "INSERT INTO tasks_dicts(net_id, dict_id, status) VALUES('" . getLastNetID() . "', '" . $dict_curr_id . "', '0')";
+			$mysqli->query( $sql );
+		}
 	}
 }
 
@@ -161,6 +270,7 @@ function getLastNetID() {
 	$result = $result->fetch_assoc();
 	return $result[ 'MAX(id)' ];
 }
+
 //Upoad file to server, check it, move it, add to DB
 //List of errors
 $errors = [
@@ -183,7 +293,7 @@ if ( isset( $_POST[ 'buttonUploadFile' ] ) ) {
 	}
 
 	//Allow hccap and cap file format only
-	$allow_fromat = array( "hccap", "cap" );
+	$allow_fromat = array( "hccap", "cap", "hccapx" );
 	if ( !in_array( $uploadFileType, $allow_fromat ) ) {
 		$uploadCode = 4;
 	}
@@ -203,15 +313,19 @@ if ( isset( $_POST[ 'buttonUploadFile' ] ) ) {
 			$file = [
 				"taskName" => $_POST[ 'task_name' ],
 				"fileName" => $_FILES[ "upfile" ][ "name" ],
-				"server_path" => $path,
+				"server_path_toFile" => $path,
+				"server_path" => $cfg_tasks_targetFolder,
 				"site_path" => $cfg_site_url . "tasks/",
 				"ext" => $uploadFileType,
 			];
 
 			$conv = handshakeConverter( $file );
+
 			if ( $conv != NULL ) {
 				foreach ( $conv as $handshake ) {
-					$info = getHandshakeInfo( $file[ 'server_path' ], false );
+					$file[ 'server_path_toFile' ] = $handshake[ "path" ];
+					$file[ 'fileName' ] = $handshake[ "name" ];
+					$info = getHandshakeInfo( $handshake[ "path" ], false, $handshake[ 'ext' ] );
 					addTaskToDB( $file, $info );
 				}
 			}
