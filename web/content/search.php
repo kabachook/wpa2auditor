31e7a1f836513feb643c232435e490b32266f036
Beltyukov Danil <beltyukovdanil@gmail.com>
2017-05-13 18:18:25 +0300

    Improved search query

--- web/content/search.php
+++ web/content/search.php
@@ -30,7 +30,8 @@
 					}
 
 					//Show tasks from DB
-					$sql = "SELECT id, name, filename, status, agents, net_key, essid, station_mac, site_path FROM tasks WHERE essid='" . $essid . "'";
+					//$sql = "SELECT id, name, filename, status, agents, net_key, essid, station_mac, site_path FROM tasks WHERE essid='" . $essid . "'"; //match whole essid only
+                    $sql = "SELECT id, name, filename, status, agents, net_key, essid, station_mac, site_path FROM tasks WHERE essid LIKE '%" . $essid . "%'";
 					$result = $mysqli->query( $sql )->fetch_all( MYSQLI_ASSOC );
 
 					$id = 0;
