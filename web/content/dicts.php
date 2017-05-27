e0be8cdf30b95b14ea028bf247a3b95b81b00c8d
0MazaHacka0 <killerinshadow2@gmail.com>
2017-05-27 18:31:24 +0300

    fix dicts.php and dicts.js. Now it works fine with tasks.js

--- web/content/dicts.php
+++ web/content/dicts.php
@@ -1,9 +1,11 @@
-﻿<?php 
+<?php 
 //Shut down error reporting
 //FOR DEV ONLY
-//error_reporting(0);
-include('Dictionary.class.php');
+error_reporting(0);
+include('..\Model/Dictionary.class.php');
+include('..\common.php');
 
+global $admin;
 
 $error_message = [
 	'code' => 0,
@@ -11,33 +13,39 @@ $error_message = [
 	"type" => "success"
 ];
 
-if (isset($_POST['buttonUploadFile'])) {
-	
+if (isset($_POST['buttonUploadDict'])) {
 	try {
-	
-		$Dict = new Dictionary($_FILES['upfile'], $_POST['filename']);
+		$Dict = Dictionary::get_dict_from_file($_FILES['upfile'], $_POST['dict_name']);
 	} catch(Exception $e) {
 		$error_message[ 'code' ] = $e->getCode();
-			$error_message[ 'message' ] = $e->getMessage();
-			$error_message[ 'type' ] = "danger";
+		$error_message[ 'message' ] = $e->getMessage();
+		$error_message[ 'type' ] = "danger";
 	}
-	
 }
 
-function getSizeHum($size) {
-	//get size in bytes
-
-	$sizes = [
-		'k', 'M', 'G', 'T'
-	];
-
-	$res;
-	$i = -1;
-	while ($size > 100) {
-		$size /= 1024;
-		$i++;
+if ($_GET['ajax'] == 'table') {
+	
+	header( 'Content-Type: application/json' );
+	
+	$ajax = [];
+	
+	$sql = "SELECT id FROM dicts";
+	$result = $mysqli->query($sql)->fetch_all(MYSQL_ASSOC);
+	foreach ($result as $dict) {
+		$Dict = Dictionary::get_dict_from_db($dict['id']);
+		array_push($ajax, $Dict->get_all_info());
 	}
-	return round($size, 2) . " " . $sizes[$i] . "B";
+	
+	$json['admin'] = $admin;
+	$json['table'] = $ajax;
+	
+	echo json_encode($json);
+	exit();
+}
+
+if($_GET['ajax'] == 'statusDictUpload') {
+	echo json_encode($error_message);
+	exit();
 }
 
 ?>
@@ -45,38 +53,22 @@ function getSizeHum($size) {
 <div class="container">
 	<div class="row">
 		<div class="col-md-8">
-			<h2>Wordlist</h2>
-			<div class="panel panel-default">
-				<table class="table table-striped table-bordered table-nonfluid">
-					<tbody align="center">
-						<tr>
-							<th>Name</th>
-							<th>Size</th>
-							<th>Download</th>
-							<?php if($admin) echo "<th>admin</th>"; ?>
-						</tr>
-						<?php
-						//Show dicts from DB 
-						$sql = "SELECT * FROM dicts WHERE 1";
-						$result = $mysqli->query($sql);
-						$result = $result->fetch_all(MYSQLI_ASSOC);
 
-						foreach ($result as $row) {
-							$str = '<tr><td><strong>' . $row['dict_name'] . '</strong></td><td>' . getSizeHum($row['size']) . '</td><td><a href="' . $row['site_path'] . '" class="btn btn-default">DOWNLOAD</a></td>';
-							$adm_str = '<td><form action="" method="post"><input type="hidden" name="deleteDictID" value="' . $row['id'] . '"><button type="submit" class="btn btn-default" name="deleteDict"><span class="glyphicon glyphicon-trash"></span></button></form></td>';
-							echo $str;
-							if ($admin)
-								echo $adm_str;
-							echo "</tr>";
-						}
-						?>
-					</tbody>
-				</table>
+			<!-- Header -->
+			<h2>Dictionaries</h2>
+			<!-- Header end -->
+
+			<!-- Table -->
+			<div id="ajaxTableDiv">
 			</div>
+			<!-- Table end -->
+		
 		</div>
+
+		<!-- side bar start -->
 		<div class="col-md-4">
 			<h2>Add new wordlist</h2>
-			<form class="" action="" method="post" enctype="multipart/form-data">
+			<form class="" action="" method="post" enctype="multipart/form-data" onSubmit="Dictionary.ajaxUploadDict(this);" id="formUploadDictionary">
 				<input type="hidden" name="source" value="upload">
 				<input type="hidden" name="action" value="addfile">
 				<div class="panel panel-default">
@@ -87,7 +79,7 @@ function getSizeHum($size) {
 							</tr>
 							<tr>
 								<td>
-									<input type="text" class="form-control" name="filename" required="" placeholder="Enter filename">
+									<input type="text" class="form-control" name="dict_name" required="" placeholder="Enter filename">
 								</td>
 							</tr>
 							<tr>
@@ -100,13 +92,16 @@ function getSizeHum($size) {
 									<input type="submit" class="btn btn-secondary" value="Upload files" name="buttonUploadFile">
 								</td>
 							</tr>
-														<tr>
-								<?php echo $error_message['message']; ?>
-							</tr>
 						</tbody>
 					</table>
 				</div>
 			</form>
 		</div>
+		<!-- side bar end -->
+		
+	</div>
+</div>
+
+		
 	</div>
 </div>
